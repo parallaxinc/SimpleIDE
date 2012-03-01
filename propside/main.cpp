8b04014c2c355ece4152b6a3caa39966c6ba645f
Steve Denson <jsdenson@gmail.com>
2012-02-29 20:20:45 -0800

    remove non-working startup splash

--- propside/main.cpp
+++ propside/main.cpp
@@ -1,18 +1,11 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include "Sleeper.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-
-    QPixmap pixmap(":/images/SimpleIDE-splash.png");
-    QSplashScreen splash(pixmap);
-    splash.setWindowFlags(Qt::WindowStaysOnTopHint);
-    splash.show();
-    a.processEvents();
-
     MainWindow w;
     w.show();
-    splash.finish(&w);
     return a.exec();
 }
