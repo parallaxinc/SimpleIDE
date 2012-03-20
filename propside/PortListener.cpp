416210b6bcec72dbb6e8deebab538158ae066b8a
Steve Denson <jsdenson@gmail.com>
2012-03-19 17:59:11 -0700

    Needed to handle \r in terminal. Set timeout to small value.

--- propside/PortListener.cpp
+++ propside/PortListener.cpp
@@ -32,7 +32,7 @@ void PortListener::init(const QString & portName, BaudRateType baud)
     port->setParity(PAR_NONE);
     port->setDataBits(DATA_8);
     port->setStopBits(STOP_1);
-    port->setTimeout(500);
+    port->setTimeout(10);
 }
 
 void PortListener::setDtr(bool enable)
