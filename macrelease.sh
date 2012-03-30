e5654b137a59d256212efd300c0ea139d4357d71
Steve Denson <jsdenson@gmail.com>
2012-03-30 15:55:27 -0700

    Create macrelease.sh script.

---
+++ macrelease.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+#
+# to use this release script, QtSDK Desktop qmake must be in your PATH
+#
+DMG=SimpleIDE.dmg
+PROPGCC=/opt/parallax
+
+if [ -e ${DMG} ]; then
+   rm ${DMG}
+fi
+
+cd propside
+qmake -config release
+if test $? != 0; then
+   echo "qmake config failed."
+   exit 1
+fi
+
+#make clean
+make
+if test $? != 0; then
+   echo "make failed."
+   exit 1
+fi
+
+cd ..
+mkdir -p SimpleIDE
+if test $? != 0; then
+   echo "mkdir SimpleIDE failed."
+   exit 1
+fi
+
+cd SimpleIDE
+rm -rf SimpleIDE.app
+cp -r ../propside/SimpleIDE.app .
+if test $? != 0; then
+   echo "copy SimpleIDE.app failed."
+   exit 1
+fi
+
+macdeployqt SimpleIDE.app
+if test $? != 0; then
+   echo "macdeployqt SimpleIDE.app failed."
+   exit 1
+fi
+
+rm -rf parallax
+cp -r ${PROPGCC} .
+if test $? != 0; then
+   echo "copy ${PROPGCC} failed."
+   exit 1
+fi
+
+cp -r ../propside-demos/ demos
+if test $? != 0; then
+   echo "copy propside-demos failed."
+   exit 1
+fi
+
+@echo "run hdiutil to make a .dmg"
+@echo "hdiutil create -format UDBZ -quiet -srcfolder SimpleIDE ${DMG}"
+if test $? != 0; then
+   echo "hdiutil create ${DMG} failed."
+   exit 1
+fi
+exit 0
