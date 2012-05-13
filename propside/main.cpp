0e86149f691c6bacdf491c81083924d1d2a6bb02
Steve Denson <jsdenson@gmail.com>
2012-05-13 12:58:22 -0700

    Translator now looks only for system.locale. Add tr for properties.cpp. Update zh_SG translation.

--- propside/main.cpp
+++ propside/main.cpp
@@ -11,21 +11,23 @@ int main(int argc, char *argv[])
     dir = dir.mid(0,dir.lastIndexOf("/"));
     QString transpath = dir+"/translations/";
 
-    // TODO: remove later. only for testing with Kenichi's translations at the moment
-    QLocale locale(QLocale::Chinese, QLocale::Singapore);
-
     QTranslator qtTranslator;
     QString progName = QString(ASideGuiKey)+"_";
 
     qDebug() << transpath+progName+QLocale::system().name()+".qm";
-    qDebug() << transpath+progName+locale.name()+".qm";
 
     if(qtTranslator.load(progName + QLocale::system().name(), transpath)) {
         a.installTranslator(&qtTranslator);
     }
+
+    /*
     else if(qtTranslator.load(progName + locale.name(), transpath)) {
+        // TODO: remove later. only for testing with Kenichi's translations at the moment
+        QLocale locale(QLocale::Chinese, QLocale::Singapore);
+        qDebug() << transpath+progName+locale.name()+".qm";
         a.installTranslator(&qtTranslator);
     }
+    */
 
     MainWindow w;
     w.show();
