ba1260f51793bd030a72c62b1ee026e6dba41062
Steve Denson <jsdenson@gmail.com>
2012-12-21 13:30:45 -0800

    Fixed new project startup compiler type. Fixed memory model names.

--- propside/projectoptions.h
+++ propside/projectoptions.h
@@ -31,6 +31,11 @@ public:
     static const QString lflags;
     static const QString board;
 
+    static const QString C_COMPILER;
+    static const QString CPP_COMPILER;
+    static const QString SPIN_COMPILER;
+
+
     enum { TAB_OPT=0, TAB_C_COMP, TAB_C_LIB, TAB_SPIN_COMP };
 
     explicit ProjectOptions(QWidget *parent = 0);
