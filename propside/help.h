6e76f18d5fc87d43682690c7901bb49b878601ae
Steve Denson <jsdenson@gmail.com>
2014-07-05 11:32:18 -0700

    Add funciton help browser.

--- propside/help.h
+++ propside/help.h
@@ -4,25 +4,46 @@
 #include <QtWidgets>
 #include <QTextBrowser>
 #include <QVBoxLayout>
+#include <QToolBar>
 #include <QDialog>
+#include <QObject>
 
 class Help : public QDialog
 {
     Q_OBJECT
+
 public:
     Help();
     ~Help();
     void show(QString path, QString text);
 
 private:
-    void openAddress(QString path, QString address);
+    void addToolButton(QToolBar *bar, QToolButton *btn, QString imgfile, const char *signal = 0, const char *slot = 0);
+    void openAddress(QString address);
 
     QVBoxLayout *blay;
+    QToolBar    *btool;
     QTextBrowser *bedit;
-    QDialogButtonBox *btnBox;
+
+    QStringList *history;
+    int          hisIndex;
+    QToolButton *btnBack;
+    QToolButton *btnFwd;
+    QComboBox   *cbHelpMode;
+    QLineEdit   *leHelpSearch;
+
+    QString     homeAddress;
 
 public slots:
     void accept();
+    void home();
+    void back();
+    void forward();
+    void index();
+    void search();
+    void helpModeChanged(int n);
+    void helpSearchChanged(QString text);
+    void editChanged(QUrl url);
 };
 
 #endif // HELP_H
