6ece4336f8b6cf333111438787f766bc5c0b2eb6
dgately <dgately@gmail.com>
2014-08-06 16:30:04 -0700

    fixed the usage display to conform to the macdeployqt_fix_frameworks.sh script

--- macappcodesign.sh
+++ macappcodesign.sh
@@ -6,6 +6,8 @@
 #
 # Assumes the app bundle is within a directory with the app's base name 
 #
+# Using sudo ./macappcodesign.sh is strongly recommended
+#
 # Example usage:
 # ./macappcodesign -s "Developer ID Application" -a SimpleIDE/SimpleIDE.app
 #
@@ -23,12 +25,12 @@ codesigns a Mac application (defaults to SimpleIDE.app)
 OPTIONS:
     -h          show usage
     -s identity developer certificate key   - example: -s "Developer ID Application" (default)
-    -a name     app name                    - example: -a SimpleIDE (default)
+    -n name     app name                    - example: -n SimpleIDE (default)
     -?          show usage
 EOF
 }
 
-while getopts "h:s:a:?" OPTION
+while getopts "h:s:n:?" OPTION
 do
     case $OPTION in
         h)
@@ -37,9 +39,11 @@ do
             ;;
         s)
             IDENTITY=$OPTARG
+            echo "overriding Identity default with: \"${IDENTITY}\""
             ;;
-        a)
+        n)
             APPNAME=$OPTARG
+            echo "overriding app name default with: \"${APPNAME}\""
             ;;
         ?)
             usage
@@ -55,13 +59,17 @@ then
      exit 1
 fi
 
+# 
+
 # App path must exist
 if [[ ! -d ${APPNAME}/${APPNAME}.app ]]
 then
-    echo ${APPNAME}.app does not exist!
+    echo "[Error] ${APPNAME}.app does not exist!"
     echo
     usage
     exit 1
+else
+    echo "  App name: \"${APPNAME}\" found..."
 fi
 
 # use security utility to determine if the identity is valid
@@ -69,9 +77,9 @@ fi
 SECUREID=`security find-certificate -c "$IDENTITY" | grep labl`
 if [[ -n ${SECUREID} ]]
 then
-    echo "Identity: \"${IDENTITY}\" found..."
+    echo "  Identity: \"${IDENTITY}\" found..."
 else
-    echo Identity: \"${IDENTITY}\" does not exist!
+    echo "[Error] Identity: \"${IDENTITY}\" does not exist!"
     echo
     usage
     exit 1
